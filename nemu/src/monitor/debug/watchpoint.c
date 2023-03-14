342f16a93f8d13ae49443ee295bd10fdf4ee0eb7
tracer-ics2017 <tracer@njuics.org>
2023-03-14 17:15:20 +0800

    >  compile 161220000 civilizwa Linux LAPTOP-MS4Q4BJC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:15:17 up 12:11,  0 users,  load average: 0.33, 0.18, 0.12 c29912d62eeefaf490a389d6c265377394aaddee

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -5,12 +5,14 @@
 
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
-
+WP* new_wp();
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].exp[0] = '\0';
+    wp_pool[i].value = -1;
   }
   wp_pool[NR_WP - 1].next = NULL;
 
@@ -19,5 +21,30 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp() {
+  if (free_ == NULL) {
+    assert(0);
+  }
+
+  WP *wp = free_;
+  free_ = free_->next;
+  wp->next = NULL;
+
+  return wp;
+}
+void display_wp() {
+  if (head == NULL) {
+    printf("There is no watchpoint!\n");
+    return ;
+  }
+
+  WP *wp;
+  printf("NO      expression        value\n");
+  wp = head;
+  while (wp != NULL) {
+    printf("%-5d   %-15s   %-16u\n", wp->NO, wp->exp, wp->value);
+    wp = wp->next;
+  }
+}
 
 
