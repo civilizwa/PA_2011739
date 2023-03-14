c86ced50f8b4a881ee2fcf11e80bd5a4de801f37
tracer-ics2017 <tracer@njuics.org>
2023-03-14 16:44:08 +0800

    >  compile 161220000 civilizwa Linux LAPTOP-MS4Q4BJC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:44:05 up 11:40,  0 users,  load average: 0.87, 0.51, 0.23 195ca1f3ed5d2daf132cb3642acd41b5da3070e1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,7 +38,7 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 static int cmd_si(char *args);
-static int cmd_info(char *args);
+//static int cmd_info(char *args);
 
 
 static struct {
