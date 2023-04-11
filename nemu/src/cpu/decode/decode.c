1653f40566c6281db2c6b5ecd2965f8ec9ffd193
tracer-ics2017 <tracer@njuics.org>
2023-04-11 11:41:42 +0800

    >  compile 161220000 civilizwa Linux LAPTOP-MS4Q4BJC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:41:40 up 18:12,  0 users,  load average: 0.18, 0.17, 0.12 6c447f55fc8f75023d21ffa666f541b8eb4a0e05

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,10 +38,9 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm=instr_fetch(eip,op->width);
-  if(op->simm==1){
-    op->simm=(int8_t)op->simm;
-  }
+  t0 = instr_fetch(eip,op->width);
+  rtl_sext(&t0,&t0,op->width);
+  op->simm = t0;
 
   rtl_li(&op->val, op->simm);
 
