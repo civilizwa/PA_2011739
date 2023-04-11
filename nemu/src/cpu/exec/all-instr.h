44ae9b7d0d040140d1072a91c09e56007aa1e8aa
2011739-Shi Jiaqi <2243987073@qq.com>
2023-04-11 19:14:33 +0800

    add data-mov.c

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -14,3 +14,17 @@ make_EHelper(jcc);
 make_EHelper(jmp); 
 make_EHelper(call_rm);
 make_EHelper(jmp_rm);
+
+// data-mov.c
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(lea);
+make_EHelper(movzx);
+make_EHelper(movsx);
+make_EHelper(cltd);
+make_EHelper(cwtl);
+make_EHelper(leave);
+make_EHelper(pusha);
+make_EHelper(popa);
+make_EHelper(mov_cr2r);
+make_EHelper(mov_r2cr);
