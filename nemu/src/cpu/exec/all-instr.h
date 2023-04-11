00b67c21ab1fdf023049cc0740f45ec8c6a33fbd
2011739-Shi Jiaqi <2243987073@qq.com>
2023-04-11 19:11:19 +0800

    add control.c
    添加控制相关指令title

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -7,10 +7,10 @@ make_EHelper(operand_size);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
 
+// control.c
 make_EHelper(call);
-make_EHelper(push);
-make_EHelper(sub);
-make_EHelper(xor);
-make_EHelper(pop);
-make_EHelper(ret);
-make_EHelper(jmp);
+make_EHelper(ret); 
+make_EHelper(jcc); 
+make_EHelper(jmp); 
+make_EHelper(call_rm);
+make_EHelper(jmp_rm);
