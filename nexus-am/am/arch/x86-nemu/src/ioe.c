ff2cffaf52241fe084901474c672312244b158a8
tracer-ics2017 <tracer@njuics.org>
2023-04-13 21:01:22 +0800

    >  run 161220000 civilizwa Linux LAPTOP-MS4Q4BJC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:01:20 up 1 day, 12:48,  0 users,  load average: 0.14, 0.10, 0.09 c1d310cef2a540dac69e06351ffa513ddb7d1952

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,8 +9,7 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  unsigned long ms=inl(RTC_PORT)-boot_time;
-  return ms;
+  return 0;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
@@ -23,17 +22,9 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  /*
   int i;
   for (i = 0; i < _screen.width * _screen.height; i++) {
     fb[i] = i;
-  }*/
-  int temp=(w>_screen.width-x)?_screen.width-x:w;
-  int cp_bytes=sizeof(uint32_t)*temp;
-  for(int i=0;i<h&&y+i<_screen.height;i++)
-  {
-      memcpy(&fb[(y+i)*_screen.width+x],pixels,cp_bytes);
-      pixels+=w;
   }
 }
 
@@ -41,8 +32,5 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  if(inb(0x64))
-  {   return inl(0x60);}
-  else
-  {   return _KEY_NONE;}
+  return _KEY_NONE;
 }
