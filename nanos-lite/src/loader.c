b95431801acacdf34bf33f103a2cc642064eda21
tracer-ics2017 <tracer@njuics.org>
2023-05-09 14:29:08 +0800

    >  compile 2011739 civilizwa Linux LAPTOP-MS4Q4BJC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:29:04 up 16:16,  0 users,  load average: 0.74, 0.62, 0.41 c7a6671633674b6944935f74532f7f8fc8b407e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,10 +11,6 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
-  int fd = fs_open(filename, 0, 0);
-  Log("filename=%s,fd=%d",filename,fd);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
-  fs_close(fd);
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
