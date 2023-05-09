be6cb62046f6c5add818ea647b4550cbd9f4c527
tracer-ics2017 <tracer@njuics.org>
2023-05-09 15:04:35 +0800

    >  compile 2011739 civilizwa Linux LAPTOP-MS4Q4BJC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:04:30 up 16:52,  0 users,  load average: 0.49, 0.32, 0.26 6c1d525f82142316847d7c62b8204d76b8ba5f1b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,22 +10,6 @@ void sys_exit(int a){
   _halt(a);
 }
 
-int sys_write(int fd, void *buf, size_t len) {
-	if(fd == 1 || fd == 2){
-		char c;
-    // Log("buffer:%s", (char*)buf);
-		for(int i = 0; i < len; i++) {
-			memcpy(&c ,buf + i, 1);
-			_putc(c);
-		}
-		return len;
-	}
-  if(fd >= 3) {
-    return fs_write(fd, buf, len);
-  }
-  Log("fd <= 0");
-	return -1;			
-}
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -38,9 +22,6 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_exit: 
       sys_exit(a[1]);
-      break;
-    case SYS_write:
-      SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
