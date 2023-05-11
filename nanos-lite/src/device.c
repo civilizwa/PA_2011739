7d249c2bff3c1c8ab7a8ad68cf5870af02f7d59d
2011739-Shi Jiaqi <2243987073@qq.com>
2023-05-11 16:28:54 +0800

    Update device.c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -18,6 +18,16 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  int index, screen_x, screen_y;
+  int w = 0; 
+  int h = 0;
+  getScreen(&w, &h);
+  for(int i = 0; i < len / 4; i++) {
+    index = offset / 4 + i;
+    screen_y = index / w;
+    screen_x = index % w;
+    _draw_rect(buf + i *4, screen_x, screen_y, 1, 1);
+  }
 }
 
 void init_device() {
