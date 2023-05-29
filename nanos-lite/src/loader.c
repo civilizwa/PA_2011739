33550f9ceba4f0c714d153576788a85a3ce192b8
2011739-Shi Jiaqi <2243987073@qq.com>
2023-05-29 16:11:45 +0800

    Update loader.c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,7 +6,7 @@ extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
-
+#define DEFAULT_ENTRY ((void *)0x8048000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
   int fd = fs_open(filename, 0, 0);
