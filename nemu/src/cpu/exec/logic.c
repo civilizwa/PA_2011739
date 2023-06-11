edfe2e629a200759ffbb72fa7323fed66cf2597e
2011739-Shi Jiaqi <2243987073@qq.com>
2023-06-11 15:26:43 +0800

    add shrd

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -122,3 +122,21 @@ make_EHelper(shld) {
 
   print_asm_template2(shld);
 }
+make_EHelper(shrd) {
+  rtl_shr(&t0, &id_dest->val, &id_src->val);
+  if (decoding.is_operand_size_16) {
+    rtl_addi(&t1, &tzero, 16);
+  } else {
+    rtl_addi(&t1, &tzero, 32);
+  }
+  rtl_sub(&t1, &t1, &id_src->val);
+  rtl_shl(&t2, &id_src2->val, &t1);
+
+  rtl_or(&t0, &t0, &t2);
+  operand_write(id_dest, &t0);
+
+  // unnecessary to update CF and OF in NEMU
+  rtl_update_ZFSF(&t0, id_dest->width);
+
+  print_asm_template2(shrd);
+}
