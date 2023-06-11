c0192eb1713a4973382254a8b45460175276559b
2011739-Shi Jiaqi <2243987073@qq.com>
2023-06-11 14:22:02 +0800

    Update FLOAT.h

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -12,14 +12,7 @@ static inline int F2int(FLOAT a) {
 }
 
 static inline FLOAT int2F(int a) {
-  //assert(0);
-  //return 0;
-  if(a&0x80000000==0){
-    return a>>16;
-  }
-  else{
-    return -((-a)>>16);
-  }
+  return a<<16;
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
